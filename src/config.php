754d3cd33105c380ae9b9a13d63a465158065d01
Andrew Sauder <andrew@sauder.email>
2024-03-12 18:02:41 -0400

    update library structure and add correct bridges and culverts feature service

--- src/config.php
+++ src/config.php
@@ -4,16 +4,56 @@ namespace gcgov\arcgis;
 
 final class config {
 
-	private static string $baseUrl = 'https://gis.garrettcounty.org/server/rest/services/';
+	private string $baseUrl = '';
+	private string $username = '';
+	private string $password = '';
+	private ?\gcgov\arcgis\sdk\Token $token = null;
 
+	public function __construct( string $baseUrl='https://gis.garrettcounty.org/', string $username='', string $password='' ) {
+		$this->setBaseUrl( $baseUrl );
+		$this->setUsername( $username );
+		$this->setPassword( $password );
+	}
 
-	public static function getBaseUrl(): string {
-		return self::$baseUrl;
+	public function getBaseUrl( string $append='' ): string {
+		return rtrim($this->baseUrl, '/').'/'.ltrim($append, '/');
 	}
 
 
-	public static function setBaseUrl( string $baseUrl ): void {
-		self::$baseUrl = $baseUrl;
+	public function setBaseUrl( string $baseUrl ): void {
+		$this->baseUrl = $baseUrl;
+	}
+
+
+	public function getUsername(): string {
+		return $this->username;
+	}
+
+
+	public function setUsername( string $username ): void {
+		$this->username = $username;
+	}
+
+
+	public function getPassword(): string {
+		return $this->password;
+	}
+
+
+	public function setPassword( string $password ): void {
+		$this->password = $password;
+	}
+
+	/**
+	 * @return string
+	 * @throws \GuzzleHttp\Exception\GuzzleException
+	 * @throws \andrewsauder\jsonDeserialize\exceptions\jsonDeserializeException
+	 */
+	public function getToken(): string {
+		if( !isset($this->token) || $this->token->expires<=time() ) {
+			$this->token = \gcgov\arcgis\sdk\Token::get( $this );
+		}
+		return $this->token->token;
 	}
 
 }
