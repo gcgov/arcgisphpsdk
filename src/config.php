46b87a40d26585885077040dc5401b3a0f72bd29
Andrew Sauder <andrew@sauder.email>
2024-04-08 09:21:53 -0400

    add development flag in config

--- src/config.php
+++ src/config.php
@@ -7,9 +7,10 @@ final class config {
 	private string $baseUrl = '';
 	private string $username = '';
 	private string $password = '';
+	private bool $development = false;
 	private ?\gcgov\arcgis\sdk\Token $token = null;
 
-	public function __construct( string $baseUrl='https://gis.garrettcounty.org/', string $username='', string $password='' ) {
+	public function __construct( string $baseUrl='https://gis.garrettcounty.org/', string $username='', string $password='', bool $development=false ) {
 		$this->setBaseUrl( $baseUrl );
 		$this->setUsername( $username );
 		$this->setPassword( $password );
@@ -54,6 +55,16 @@ final class config {
 			$this->token = \gcgov\arcgis\sdk\Token::get( $this );
 		}
 		return $this->token->token;
+	}
+
+
+	public function isDevelopment(): bool {
+		return $this->development;
+	}
+
+
+	public function setDevelopment( bool $development ): void {
+		$this->development = $development;
 	}
 
 }
